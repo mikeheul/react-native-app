be141383c8294ee19aa52f16c35bb5b44ba0d9a2
Micka <mickael.murmann@gmail.com>
2024-02-16 23:52:08 +0100

    init commit

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
+
+const MyModal = ({ visible, onClose }) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+      >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text>This is a modal!</Text>
+          <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleModalOpen = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText} onPress={handleModalOpen}>
+        Hello Micka
+      </Text>
+      <MyModal visible={modalVisible} onClose={handleModalClose} />
     </View>
   );
 }
@@ -16,5 +49,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 200,
+    backgroundColor: '#303043',
+  },
+
+  titleText: {
+    fontSize: 90,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    width: "80%",
+  },
+  closeButton: {
+    marginTop: 10,
   },
 });
